721876c7f00bd861fe97c0b2a4254f064bf75ff3
mylehuynh <huynhlemy78@gmail.com>
2026-03-22 16:22:11 +0700

    Add button click event to show alert message

---
+++ script.js
@@ -0,0 +1,7 @@
+// Lấy thẻ button thông qua ID
+const button = document.getElementById("myButton");
+
+// Thêm sự kiện click cho button
+button.addEventListener("click", function() {
+    alert("Cảm ơn bạn đã ghé thăm trang web của tôi! Chúc bạn một ngày tốt lành.");
+});
